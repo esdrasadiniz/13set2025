2693713ab64f55b5c5f9293e41c00fd73a4ac684
esdrasadiniz <esdras.pert@gmail.com>
2025-02-03 23:32:05 -0300

    completa pagina de inicio e loading

---
+++ js/loading.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    const loaderBg = document.querySelector(".loader-bg");
+    const titulo = document.querySelector(".main-title");
+    const flowers = document.querySelector(".flowers");
+    const casalGhibli = document.querySelector(".casal-ghibli");
+    const menu = document.querySelector(".main-menu");
+
+    // Aguarda o tempo do loader antes de iniciar a transição
+    setTimeout(function () {
+        loader.classList.add("invisible");
+        loaderBg.classList.add("invisible");
+    }, 3500)
+        // Aguarda um pequeno tempo e então anima os elementos
+        setTimeout(function () {
+            titulo.classList.add("visible");
+            casalGhibli.classList.add("visible");
+            flowers.classList.add("visible");
+            menu.classList.add("visible");
+        }, 4000); // Pequeno delay para um efeito mais suave
+
+        setTimeout(function () {
+            flowers.classList.add("overflow-hidden");
+        }, 5000);
+
+        // Remove o loader completamente após a transição
+        setTimeout(function () {
+            loader.classList.add("hidden");
+        }, 4000);
+    }, 4000); // Tempo de exibição do loader antes da transição
