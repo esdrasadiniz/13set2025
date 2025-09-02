7711839493a4a48a68805785fe49d1d356eb59bb
esdrasadiniz <esdras.pert@gmail.com>
2025-09-02 11:01:28 -0300

    js copy pix

---
+++ js/modal-pagamento.js
@@ -0,0 +1,18 @@
+ // Seleciona todos os botões de copiar
+    const botoes = document.querySelectorAll(".copiar-pix");
+
+    botoes.forEach(botao => {
+        botao.addEventListener("click", () => {
+            // pega o código do atributo data-pix
+            const codigoPix = botao.getAttribute("data-pix");
+
+            navigator.clipboard.writeText(codigoPix)
+                .then(() => {
+                    // aqui você pode mostrar uma mensagem personalizada
+                    alert("Código PIX copiado!");
+                })
+                .catch(err => {
+                    console.error("Erro ao copiar PIX: ", err);
+                });
+        });
+    });
